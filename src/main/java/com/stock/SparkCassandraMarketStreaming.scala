7a8ca3211f46e4c9b0047774b54c207d4ca68f42
suyamba <A.Suyambu@rn.rabobank.nl>
2017-02-16 13:25:32 +0100

    sub folder added

--- src/main/java/com/stock/SparkCassandraMarketStreaming.scala
+++ src/main/java/com/stock/SparkCassandraMarketStreaming.scala
@@ -14,8 +14,8 @@ import org.apache.spark.sql.SaveMode
 import com.datastax.spark.connector.cql.CassandraConnector
 import java.util.UUID
 
-case class Record2(bid_price: Double, order_quantity: Int, symbol: String, trade_type: String, timestamp: String)
-case class OutPut1(marketuuid: String, symbol: String, order_quantity: Int)
+case class MarketStreamData(bid_price: Double, order_quantity: Int, symbol: String, trade_type: String, timestamp: String)
+case class MarketOrder(marketuuid: String, symbol: String, order_quantity: Int)
 object SparkCassandraMarketStreaming {
 
   import com.datastax.spark.connector._
@@ -29,15 +29,14 @@ object SparkCassandraMarketStreaming {
 
     StreamingExamples.setStreamingLogLevels()
 
-
     val Array(zkQuorum, group, topics, numThreads) = args
     val sparkConf = new SparkConf().setAppName("JSONStreamExample").setMaster("local[2]").set("spark.driver.allowMultipleContexts", "true")
     sparkConf.set("spark.cassandra.connection.host", "127.0.0.1")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
     val sql = new SparkContext(sparkConf)
     val sqlContext = new SQLContext(sql)
-    val timer = Time(10000)
-    //    ssc.checkpoint("checkpoint")
+    //val timer = Time(10000)
+    //ssc.checkpoint("checkpoint")
 
     import sqlContext.implicits._
 
@@ -45,8 +44,7 @@ object SparkCassandraMarketStreaming {
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
     val jsonf = lines.map(JSON.parseFull(_)).map(_.get.asInstanceOf[scala.collection.immutable.Map[Any, Any]])
     jsonf.print()
-    val fields =
-      jsonf.map(data => Record2(data("bid_price").toString.toDouble, data("order_quantity").toString.toInt, data("symbol").toString, data("trade_type").toString, data("timestamp").toString))
+    val fields = jsonf.map(data => MarketStreamData(data("bid_price").toString.toDouble, data("order_quantity").toString.toInt, data("symbol").toString, data("trade_type").toString, data("timestamp").toString))
     fields.print()
 
     CassandraConnector(sparkConf).withSessionDo { session =>
@@ -55,18 +53,17 @@ object SparkCassandraMarketStreaming {
       session.execute("CREATE TABLE marketspace.market_order(marketuuid VARCHAR PRIMARY KEY, symbol VARCHAR, order_quantity INT)")
     }
 
-    val results = fields.foreachRDD((recrdd, timer) => {
-      recrdd.toDF().registerTempTable("table1");
-       import com.gilt.timeuuid._
-      val sqlreport = sqlContext.sql("select symbol, SUM(order_quantity) from table1 group by symbol order by symbol")
+    val results = fields.foreachRDD((recrdd) => {
+      recrdd.toDF().registerTempTable("marketordertable");
+      import com.gilt.timeuuid._
+      val sqlreport = sqlContext.sql("select symbol, SUM(order_quantity) from marketordertable group by symbol order by symbol")
       //sqlreport.map(t => OutPut1(t(0).toString, t(1).toString.toInt)).collect().foreach(a => {})
-     
-      sqlreport.map(t => OutPut1(TimeUuid().toString(), t(0).toString, t(1).toString.toInt)).saveToCassandra("marketspace","market_order")
+
+      sqlreport.map(t => MarketOrder(TimeUuid().toString(), t(0).toString, t(1).toString.toInt)).saveToCassandra("marketspace", "market_order")
 
       println(sqlreport)
 
     })
-
 
     ssc.start()
     ssc.awaitTermination()
